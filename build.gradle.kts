7550550494ba3963ea4303b52d42ae74057ea098
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-29 15:33:38 +0900

    [ADD] logback 설정 작성 및 prod 환경에서의 ELK연동

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+    implementation("net.logstash.logback:logstash-logback-encoder:6.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
